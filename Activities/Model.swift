8ebbd69a4c60ca365f38ba62bc988ed2b3b64a4b
Mariano Portilla <chemanoportilla@hotmail.com>
2022-05-03 03:54:10 -0500

    Add placeholder code to run the app (should delete it after)

--- Activities/Model.swift
+++ Activities/Model.swift
@@ -4,7 +4,7 @@ class Model {
     var Activities: [Activity]
     
     init() {
-        
+        Activities = []
     }
     
     func createRecordInDatabase(withId idActivity: String) {
@@ -20,14 +20,14 @@ class Model {
     }
     
     func selectAllTerminatedActivities() -> [Activity] {
-        
+        return [Activity(idActivity: "e", name: "e", description: "e", estimatedTime: 2, scheduledTime: 4, realTime: 2, isTerminated: true)]
     }
     
     func selectAllNotTerminatedActivities() -> [Activity] {
-        
+        return [Activity(idActivity: "e", name: "e", description: "e", estimatedTime: 2, scheduledTime: 4, realTime: 2, isTerminated: true)]
     }
         
     func selectActivityById(_ idActivity: String) -> Activity {
-        
+        return Activity(idActivity: "e", name: "e", description: "e", estimatedTime: 2, scheduledTime: 4, realTime: 2, isTerminated: true)
     }
 }
