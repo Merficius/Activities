d32cf58cbe331524fd365cec9b226a1046cf8bc7
Mariano Portilla <chemanoportilla@hotmail.com>
2022-05-10 01:02:12 -0500

    Add tableview custom cells to logs and add constraints

--- Activities/LogsController.swift
+++ Activities/LogsController.swift
@@ -2,9 +2,19 @@ import UIKit
 
 class LogsController: UIViewController {
 
+    @IBOutlet var LogsTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Creating the nib that holds the LogsTableViewCell.xib file
+        let nib = UINib(nibName: "LogsTableViewCell", bundle: nil)
+        
+        // Registers the custom nib so that we can use it in our tableview
+        LogsTableView.register(nib, forCellReuseIdentifier: "LogsTableViewCell")
+        
+        // Assigns the datasource to itself
+        LogsTableView.dataSource = self
     }
 
     func readTerminatedActivities() {
@@ -20,3 +30,17 @@ class LogsController: UIViewController {
     }
 }
 
+extension LogsController: UITableViewDataSource {
+    
+    // Specifies which cell is going to use and selects the data that is going to fill
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LogsTableViewCell", for: indexPath) as! LogsTableViewCell
+        
+        return cell
+    }
+    
+    // Returns the number of cells wanted
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+}
