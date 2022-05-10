925127640e19c169f5de8deee998a5fbc8fc50f3
Mariano Portilla <chemanoportilla@hotmail.com>
2022-05-10 01:14:50 -0500

    Add tableview custom cell for activities home menu

---
+++ Activities/ActivitiesTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  ActivitiesTableViewCell.swift
+//  Activities
+//
+//  Created by José Mariano Portilla Landa on 10/05/22.
+//
+
+import UIKit
+
+class ActivitiesTableViewCell: UITableViewCell {
+
+    @IBOutlet var ActivityNameLabel: UILabel!
+    @IBOutlet var ActivityDurationLabel: UILabel!
+    @IBOutlet var ActivityControlButton: UIButton!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
