5045a9787b525cd17165e40fc34cdca4e9a1d33f
Mariano Portilla <chemanoportilla@hotmail.com>
2022-05-12 06:02:31 -0500

    Delete code, refactor, and comment the model

--- Activities/Model.swift
+++ Activities/Model.swift
@@ -10,9 +10,11 @@ class Model {
     static func createRecordInDatabase(id: Int64, name: String, description: String, estimatedTime: Int64, scheduledTime: Int64, realTime: Int64, isTerminated: Bool) {
         var managedObject: NSManagedObject
 
+        // We create a managed object into the managedobject context for the correspondent entity description
         entityDescription = NSEntityDescription.entity(forEntityName: "Activity", in: managedObjectContext!)
         managedObject = NSManagedObject(entity: entityDescription!, insertInto: Model.managedObjectContext)
         
+        // Setting the values
         managedObject.setValue(id, forKey: "idActivity")
         managedObject.setValue(name, forKey: "activityName")
         managedObject.setValue(description, forKey: "activityDescription")
@@ -24,18 +26,64 @@ class Model {
         save()
     }
     
-    // Updates a record in database with all the fields as parameters exceot real time
-    static func updateRecordInDatabase(id: Int64, name: String, description: String, estimatedTime: Int64, scheduledTime: Int64, isTerminated: Bool) {
-        let activity = selectActivityById(id)
-    
-        activity.idActivity = id
-        activity.activityName = name
-        activity.activityDescription = description
-        activity.activityEstimatedTime = estimatedTime
-        activity.activityScheduledTime = scheduledTime
-        activity.activityIsTerminated = isTerminated
+    // Selects all activities that are ended
+    static func selectAllTerminatedActivities() -> [Activity] {
+        var arrayOfManagedObjects: [Activity]
         
-        save()
+        fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Activity")
+        fetchRequest.predicate = NSPredicate(format: "activityIsTerminated == true")
+        arrayOfManagedObjects = executeFetch()
+        
+        terminatedActivities = arrayOfManagedObjects
+        
+        return arrayOfManagedObjects
+    }
+    
+    // Selects all activities that are not ended
+    static func selectAllNotTerminatedActivities() -> [Activity] {
+        var arrayOfManagedObjects: [Activity]
+        
+        fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Activity")
+        fetchRequest.predicate = NSPredicate(format: "activityIsTerminated == false")
+        arrayOfManagedObjects = executeFetch()
+        
+        notTerminatedActivities = arrayOfManagedObjects
+        
+        return arrayOfManagedObjects
+    }
+    
+    // Selects an activity by it's id
+    static func selectActivityById(_ idActivity: Int64) -> Activity {
+        var arrayOfManagedObjects: [Activity]
+        
+        fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Activity")
+        fetchRequest.predicate = NSPredicate(format: "idActivity == \(idActivity)")
+        arrayOfManagedObjects = executeFetch()
+        
+        return arrayOfManagedObjects.first!
+    
+    }
+    
+    // Selects all activities ordered by a key (normally used with idActivity)
+    static func selectAllActivities(orderedBy: String) -> [Activity] {
+        var sortDescriptor: NSSortDescriptor
+        var sortDescriptors: [NSSortDescriptor]
+        var arrayOfManagedObjects: [Activity]
+        
+        fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Activity")
+        
+        // When you execute a fetch, by default returnsObjectsAsFaults is true;
+        // Core Data fetches the object data for the matching, fills the row cache with the information,
+        // and returns managed object as faults.
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        // Sort descriptor to order the fetch
+        sortDescriptor = NSSortDescriptor(key: orderedBy, ascending: false)
+        sortDescriptors = [sortDescriptor]
+        fetchRequest.sortDescriptors = sortDescriptors
+        
+        arrayOfManagedObjects = executeFetch()
+        return arrayOfManagedObjects
     }
     
     // Updates a record in database with all the fields as parameters
@@ -53,6 +101,7 @@ class Model {
         save()
     }
     
+    // Deletes a record in database with an id
     static func deleteRecordInDatabase(withId idActivity: Int64) {
         var managedObject: Activity
         
@@ -62,58 +111,7 @@ class Model {
         
     }
     
-    static func selectAllTerminatedActivities() -> [Activity] {
-        var arrayOfManagedObjects: [Activity]
-        
-        fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Activity")
-        fetchRequest.predicate = NSPredicate(format: "activityIsTerminated == true")
-        arrayOfManagedObjects = executeFetch()
-        
-        terminatedActivities = arrayOfManagedObjects
-        
-        return arrayOfManagedObjects
-    }
-    
-    static func selectAllNotTerminatedActivities() -> [Activity] {
-        var arrayOfManagedObjects: [Activity]
-        
-        fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Activity")
-        fetchRequest.predicate = NSPredicate(format: "activityIsTerminated == false")
-        arrayOfManagedObjects = executeFetch()
-        
-        notTerminatedActivities = arrayOfManagedObjects
-        
-        return arrayOfManagedObjects
-    }
-    
-    static func selectActivityById(_ idActivity: Int64) -> Activity {
-        var arrayOfManagedObjects: [Activity]
-        
-        fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Activity")
-        fetchRequest.predicate = NSPredicate(format: "idActivity == \(idActivity)")
-        arrayOfManagedObjects = executeFetch()
-        
-        return arrayOfManagedObjects.first!
-    
-    }
-    
-    static func selectAllActivities(orderedBy: String) -> [Activity] {
-        var sortDescriptor: NSSortDescriptor
-        var sortDescriptors: [NSSortDescriptor]
-        var arrayOfManagedObjects: [Activity]
-        
-        fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Activity")
-        fetchRequest.returnsObjectsAsFaults = false
-        
-        // we include a sort descriptor
-        sortDescriptor = NSSortDescriptor(key: orderedBy, ascending: false)
-        sortDescriptors = [sortDescriptor]
-        fetchRequest.sortDescriptors = sortDescriptors
-        
-        arrayOfManagedObjects = executeFetch()
-        return arrayOfManagedObjects
-    }
-    
+    // Deletes an activity with it's id
     static func deleteFromActivities(withId id: Int64) {
         var managedObject: Activity
         
@@ -195,96 +193,9 @@ extension Model {
         return arrayOfManagedObjects
     }
     
-    static func selectAllFromUsers() -> [Activity] {
-        var arrayOfManagedObjects: [Activity]
-        
-        fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Activity")
-        
-        // When you execute a fetch, by default returnsObjectsAsFaults is true;
-        // Core Data fetches the object data for the matching records,
-        // fills the row cache with the information,
-        // and returns managed object as faults.
-        fetchRequest.returnsObjectsAsFaults = false
-        
-        arrayOfManagedObjects = executeFetch()
-        return arrayOfManagedObjects
-    }
     
-    static func selectAllFromUsers(orderBy: String) -> [Activity] {
-        var sortDescriptor: NSSortDescriptor
-        var sortDescriptors: [NSSortDescriptor]
-        var arrayOfManagedObjects: [Activity]
-        
-        fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Activity")
-        fetchRequest.returnsObjectsAsFaults = false
-        // we include a sort descriptor
-        sortDescriptor = NSSortDescriptor(key: orderBy, ascending: true)
-        sortDescriptors = [sortDescriptor]
-        fetchRequest.sortDescriptors = sortDescriptors
-        
-        arrayOfManagedObjects = executeFetch()
-        return arrayOfManagedObjects
-    }
-    
-    static func selectAllFromUsersWhere(username: String) -> [Activity] {
-        var arrayOfManagedObjects: [Activity]
-        
-        fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Activity")
-        fetchRequest.predicate = NSPredicate(format: "username == %@", username)
-        arrayOfManagedObjects = executeFetch()
-        return arrayOfManagedObjects
-    }
-    
-    static func selectTop1FromUsersWhere(username: String) -> Activity {
-        var arrayOfManagedObjects: [Activity]
-        var firstObject: Activity
-        
-        fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Activity")
-        fetchRequest.predicate = NSPredicate(format: "username == %@", username)
-        arrayOfManagedObjects = executeFetch()
-        firstObject = arrayOfManagedObjects.first!
-        return firstObject
-    }
-    
-    // We create a Managed Object into the Managed Object Context
-    // for the correspondent Entity Description.
-    // Then we set the corresponding values (data).
-    static func insertIntoUsers(idActivity: Int64, name: String, description: String) {
-        //        var managedObject: NSManagedObject
-        let activity: Activity!
-        
-        //        entityDescription = NSEntityDescription.entity(forEntityName: "Activity", in: managedObjectContext!)
-        //        managedObject = NSManagedObject(entity: entityDescription!, insertInto: managedObjectContext)
-        //        managedObject.setValue(idActivity, forKey: "idActivity")
-        //        managedObject.setValue(name, forKey: "activityName")
-        //        managedObject.setValue(description, forKey: "activityDescription")
-        
-        
-        var arrayOfManagedObjects: [Activity]
-        
-        fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Activity")
-        fetchRequest.predicate = NSPredicate(format: "idActivity == %@", idActivity)
-        arrayOfManagedObjects = executeFetch()
-        
-        if arrayOfManagedObjects.count == 0 {
-            // here you are inserting
-            activity = Activity(context: managedObjectContext!)
-        } else {
-            // here you are updating
-            activity = arrayOfManagedObjects.first!
-        }
-        
-        activity.idActivity = idActivity
-        activity.activityName = name
-        activity.activityDescription = description
-        
-        save()
-    }
-    
-    
-    
-    // we did not code the Users class
-    static func deleteAllFromUsers() {
+    // This function is only for testing/developing, it deletes all activities from the database
+    static func deleteAllActivities() {
         var arrayOfManagedObjects: [Activity]
         
         arrayOfManagedObjects = selectAllActivities(orderedBy: "idActivity")
@@ -295,13 +206,6 @@ extension Model {
         }
         
         save()
-    }
-    
-    static func deleteFromUsersWhere(username: String) {
-        var managedObject: Activity
-        
-        managedObject = selectTop1FromUsersWhere(username: username)
-        managedObjectContext?.delete(managedObject)
     }
     
     // Try to save the Managed Object Context
